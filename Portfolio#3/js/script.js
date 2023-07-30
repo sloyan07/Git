6ae4102d3f3c68de0e2b7ab8a056b96675508e47
Kniaz__Sloyan <sloyan07@list.ru>
2023-07-30 03:10:31 +0300

    Portfolio#3

---
+++ Portfolio#3/js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+
+
+console.log('Я учу JS')
+console.log('учу')
+console.log('привет Князь, это ты изучаешь Git и Github?)))((');
+console.log('hello kniaz!');
+console.log('hello ёпт');
+console.log('hello'); 
+
+
+
+
+
